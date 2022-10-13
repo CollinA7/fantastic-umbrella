0210482e408ab12383da8ea64171959dee404955
Cayean <collinayala@gmail.com>
2022-10-12 22:44:24 -0500

    added category post and put requests

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,11 +35,40 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbPutData => {
+    if (!dbPutData) {
+      res.status(404).json({ message: 'There is no category with this id' });
+      return;
+    }
+    res.json(dbPutData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
