649176f3dd690597f6ad9d6acb6f5beb1d81744c
Cayean <collinayala@gmail.com>
2022-10-12 22:56:45 -0500

    added delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -73,6 +73,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbDeleteData => {
+    if (!dbDeleteData) {
+      res.status(404).json({ message: 'No category with this id' })
+      return;
+    }
+    res.json(dbDeleteData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 module.exports = router;
